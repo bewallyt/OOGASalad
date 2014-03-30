4a3a23892796dbd2e6dd24c0af0aa845ac6f272b
Lee Weisberger <lee.weisberger@duke.edu>
2014-03-30 15:48:41 -0400

    working on movement

--- src/engine/Player.java
+++ src/engine/Player.java
@@ -1,19 +1,38 @@
 package engine;
 
-import java.util.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.List;
 
-public class Player extends RuleFollower {
-	
+public class Player extends RuleFollower{
+
 	List<Item> myItems;
-	
+
 	public Player(double x, double y, double dx, double dy) {
 		super(x,y,dx,dy);
 		myItems = null;
+		
+
 	}
-	
+
 	@Override 
 	public void move() {
-		
+
 	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			myY +=myDY;
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			myY -= myDY;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			myX += myDX;
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			myX -= myDX;
+		
+		move();
+
+	}
+
 
 }
