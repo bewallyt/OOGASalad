ce83f192f24178880ff74d176a25f076f22303c0
Prit <vmpritam@gmail.com>
2014-03-30 13:02:56 -0400

    test commit

--- src/authoring/DummyClass.java
+++ src/authoring/DummyClass.java
@@ -5,4 +5,6 @@ public class DummyClass {
 	//this class doesn't do anything, only created to push the .project of the repo
 	//you should be able to directly import to eclipse now
 	// test
+	
+	public void doNothin(){}
 }
