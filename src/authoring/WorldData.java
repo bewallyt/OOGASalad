d866ea4759cf3119889a923ef738f467cf5b06ef
Prit <vmpritam@gmail.com>
2014-04-05 15:52:10 -0400

    authoringchanges

--- src/authoring/WorldData.java
+++ src/authoring/WorldData.java
@@ -1,7 +1,37 @@
 package authoring;
+import java.io.File;
+import java.util.*;
 
 public class WorldData {
-	private TileData[][] myTileDatas;
+
+	private Map<String, MapData> myLevels;
+	private Map<String, File> myImages;
+	private static final String DEFAULT_MAP = "defaultworldkey";
+	
+	public WorldData(){
+		myLevels = new HashMap<String, MapData>();
+		myLevels.put(DEFAULT_MAP, new MapData());
+		myImages = new HashMap<String, File>();
+		//More general stuff about player, game engine, etc will go here
+	}
+	
+	public File getImage(String fileName){
+		return myImages.get(fileName);
+	}
+	
+	public void saveImage(String s, File f){
+		myImages.put(s, f);
+	}
+	
+	public MapData getMap(String s){
+		return myLevels.get(s);
+	}
+	
+	protected Map<String, File> getImages(){
+		return myImages;
+	}
+	
+	/*private TileData[][] myTileDatas;
 	private int myWorldWidth;
 	private int myWorldHeight;
 	private PlayerData myPlayerData;
@@ -32,6 +62,6 @@ public class WorldData {
 		myPlayerData.setMyAnimImages(animImages);
 		System.out.println(myPlayerData.getMyImage());
 		return myPlayerData;
-	}
+	}*/
 
 }
