d72620318ebb35cc9564aa53c7c5e54f53e64e0c
Benson <benson.tran@duke.edu>
2014-04-06 15:46:31 -0400

    Added data updates

--- src/GameView/GameFrame.java
+++ src/GameView/GameFrame.java
@@ -1,5 +1,8 @@
 package GameView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import engine.Dialogue;
 import engine.collision.CollisionMatrix;
 import engine.gridobject.Barrier;
@@ -34,32 +37,13 @@ public class GameFrame extends RPGEngine {
 	}
 	
 	public void addObjects(World world){
-		addGridObject(myPlayer = initPlayer(), 3, 3);
-		
-//		hard coded		
-		BackAndForthMover bafm = myEnemy= new BackAndForthMover("rival.png",1,1,1, 350, 550, 0, 0, myPlayer);
-		addGridObject(bafm,10,10);
-		bafm.addDialogue("Hey Bitch. Fight Me!");
-		addGridObject(new Barrier("pokecenter.png",4, 4), 4, 3);
 
-		for(int i=0; i<world.getTileGridHeight(); i++){
-			addGridObject(new Barrier("tree.png",1,2), i, 0);
-			addGridObject(new Barrier("tree.png",1,2), i, world.getTileGridHeight()-1-1);
-		}
-		for(int i=0; i<world.getTileGridWidth(); i++){
-			addGridObject(new Barrier("tree.png",1,2), 0, i);
-			addGridObject(new Barrier("tree.png",1,2), world.getTileGridWidth()-1,i );
-		}
-		
-//		work in progress till Data and authoring are ready
-
-		/*
 		TileData currTile;
-		GridObjectData[] currGridObjectDatas;
+		List<GridObjectData> currGridObjectDatas = new ArrayList<GridObjectData>();
 		
-		for(int i = 0; i < myWorldData.getWorldSize()[0]; i++){
-			for(int j = 0; j < myWorldData.getWorldSize()[1]; j++){
-				currTile = myWorldData.getTileData(i, j);
+		for(int i = 0; i < myWorldData.getMap("defaultworldkey").getMapLength(); i++){
+			for(int j = 0; j < myWorldData.getMap("defaultworldkey").getMapWidth(); j++){
+				currTile = myWorldData.getMap("defaultworldkey").getTileData(i, j);
 				currGridObjectDatas = currTile.getGridObjectDatas();
 
 				for(int k = 0; k < currGridObjectDatas.length; k++){
@@ -68,7 +52,7 @@ public class GameFrame extends RPGEngine {
 				}
 			}
 		} 
-		*/
+		
 
 	}
 
@@ -86,7 +70,9 @@ public class GameFrame extends RPGEngine {
 			myEnemy.doNextDialogue();
 	}
 	
-	private Player initPlayer() {
+	private Player initPlayer(WalkAroundWorld world) {
+		
+		
 
 //		hard coded
 		String[] animImages = new String[12];
@@ -94,7 +80,7 @@ public class GameFrame extends RPGEngine {
 		animImages[3] = "ls.png";
 		animImages[6] = "bs.png";
 		animImages[9] = "rs.png";
-		Player player = new Player("player.png",2,spriteWidth, spriteHeight);
+		Player player = new Player("player.png",2,spriteWidth, spriteHeight, null);
 		player.getAnimImages(animImages);
 		return player;
 
@@ -109,19 +95,19 @@ public class GameFrame extends RPGEngine {
 		*/
 	}
 
-//	public static void main(String[] args) {
-//		GameFrame game = new GameFrame();
-//		try {
-//			game.doGameLoop();
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		//		Canvas canvas = new Canvas (800,800);
-//		//		WalkAroundWorld waWorld = new WalkAroundWorld(40, canvas.getWidth(), canvas.getHeight());
-//		//		canvas.setWorld(waWorld);
-//		//		engine.addObjects(waWorld);
-//		//		CollisionMatrix cm = new CollisionMatrix(waWorld.getGridObjectList());
-//		//		engine.doGameLoop(waWorld, cm);
-//	}
+	public static void main(String[] args) {
+		GameFrame game = new GameFrame();
+		try {
+			game.doGameLoop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+//				Canvas canvas = new Canvas (800,800);
+//				WalkAroundWorld waWorld = new WalkAroundWorld(40, canvas.getWidth(), canvas.getHeight());
+//				canvas.setWorld(waWorld);
+//				engine.addObjects(waWorld);
+//				CollisionMatrix cm = new CollisionMatrix(waWorld.getGridObjectList());
+//				engine.doGameLoop(waWorld, cm);
+	}
 
 }
