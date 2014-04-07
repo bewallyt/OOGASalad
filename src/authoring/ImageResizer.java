901c25aefb638df7bacfe12ec1ce2159484fb29a
Prit <vmpritam@gmail.com>
2014-04-06 21:53:21 -0400

    change back to file from image

--- src/authoring/ImageResizer.java
+++ src/authoring/ImageResizer.java
@@ -9,23 +9,24 @@ import javax.imageio.ImageIO;
 
 public class ImageResizer {
 	
-	private WorldData myWorldData;
 	private Image scaledImage;
 	private String fileName;
+	private File imageFile;
 	
-	public ImageResizer(){	
-		myWorldData = new WorldData();		
-	}
+	public ImageResizer(){}
 
 	protected void squareImage(String name, File file) throws IOException {
 		fileName = name;
-		BufferedImage bimg = ImageIO.read(file);
-		scaledImage = bimg.getScaledInstance(48, 48, Image.SCALE_FAST);
+		imageFile = file;
+		
+		//BufferedImage bimg = ImageIO.read(file);
+		//scaledImage = bimg.getScaledInstance(48, 48, Image.SCALE_FAST);
+		
 		addToWorldData();
 	}
 
 	private void addToWorldData() {
-		myWorldData.saveImage(fileName, scaledImage);
+		FeatureManager.getWorldData().saveImage(fileName, imageFile);
 	}
 
 }
