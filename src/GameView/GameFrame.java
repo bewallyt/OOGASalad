96d671ea157c18b1850c26ebc6bda044ea21f32c
bchao <brandonchao3@gmail.com>
2014-04-08 19:03:35 -0400

    Making gameframe general...waiting on reflection

--- src/GameView/GameFrame.java
+++ src/GameView/GameFrame.java
@@ -34,65 +34,68 @@ public class GameFrame extends RPGEngine {
 	public GameFrame() {
 		myData = new DataDummy();
 		myWorldData = myData.getWorldData();
-
 		initializeGame();
 	}
 
-	public void addObjects(World world){
-		
+	public void addObjects(World world) {
+
 		// setting background image for tiles
 		for (int i = 0; i < world.getTileGridWidth(); i++) {
 			for (int j = 0; j < world.getTileGridHeight(); j++) {
 				world.getTileMatrix()[i][j].setBackgroundImage("grass.jpg");
 			}
 		}
-		
+
 		initPlayer();
 		addGridObject(getPlayer(), 3, 3);
 
-		NPC bafm = myNPC= new NPC(new String[] {"rival.png","rival.png","rival.png","rival.png"},1,1,1, 3, getPlayer());
-		addGridObject(bafm,10,10);
+		NPC bafm = myNPC = new NPC(new String[] { "rival.png", "rival.png",
+				"rival.png", "rival.png" }, 1, 1, 1, 3, getPlayer());
+		addGridObject(bafm, 10, 10);
 		bafm.addDialogue("Hey bitch fight me");
 		bafm.addDialogue("okay?");
 
-		addGridObject(new Barrier("pokecenter.png",4, 4), 4, 3);
+		addGridObject(new Barrier("pokecenter.png", 4, 4), 4, 3);
 
-		for(int i=0; i<world.getTileGridHeight(); i++){
-			addGridObject(new Barrier("tree.png",1,2), i, 0);
-			addGridObject(new Barrier("tree.png",1,2), i, world.getTileGridHeight()-1-1);
+		for (int i = 0; i < world.getTileGridHeight(); i++) {
+			addGridObject(new Barrier("tree.png", 1, 2), i, 0);
+			addGridObject(new Barrier("tree.png", 1, 2), i,
+					world.getTileGridHeight() - 1 - 1);
 		}
-		for(int i=0; i<world.getTileGridWidth(); i++){
-			addGridObject(new Barrier("tree.png",1,2), 0, i);
-			addGridObject(new Barrier("tree.png",1,2), world.getTileGridWidth()-1,i );
+		for (int i = 0; i < world.getTileGridWidth(); i++) {
+			addGridObject(new Barrier("tree.png", 1, 2), 0, i);
+			addGridObject(new Barrier("tree.png", 1, 2),
+					world.getTileGridWidth() - 1, i);
 		}
-		
-
-		
-//		work in progress till Data and authoring are ready
-
-
-//	public void addObjects(World world) {
-//
-//		TileData currTile;
-//		List<GridObjectData> currGridObjectDatas = new ArrayList<GridObjectData>();
-//
-//		for (int i = 0; i < myWorldData.getMap("defaultworldkey")
-//				.getMapLength(); i++) {
-//			for (int j = 0; j < myWorldData.getMap("defaultworldkey")
-//					.getMapWidth(); j++) {
-//				currTile = myWorldData.getMap("defaultworldkey").getTileData(i,
-//						j);
-//				currGridObjectDatas = currTile.getGridObjectDatas();
-//
-//				for (int k = 0; k < currGridObjectDatas.size(); k++) {
-//					// need to figure out stuff here
-//					addGridObject(new GridObject("", 0, 0));
-//				}
-//			}
-//		}
-
 	}
-	
+
+	// work in progress till Data and authoring are ready
+
+	public void addObjects2() {
+
+		TileData currTile;
+		List<GridObjectData> currGridObjectDatas = new ArrayList<GridObjectData>();
+
+		// set background images???
+		
+		// extract relevant data from myWorldData
+		for (int i = 0; i < myWorldData.getMap("defaultworldkey")
+				.getMapLength(); i++) {
+			for (int j = 0; j < myWorldData.getMap("defaultworldkey")
+					.getMapWidth(); j++) {
+				currTile = myWorldData.getMap("defaultworldkey").getTileData(i,
+						j);
+				currGridObjectDatas = currTile.getGridObjectDatas();
+
+				for (int k = 0; k < currGridObjectDatas.size(); k++) {
+					// need to figure out stuff here
+					// use reflection??
+					// addGridObject(new GridObject("", 0, 0));
+				}
+			}
+		}
+	}
+
 	public Canvas getMyCanvas() {
 		return retMyCanvas();
 	}
@@ -115,12 +118,14 @@ public class GameFrame extends RPGEngine {
 	private void initPlayer() {
 
 		// hard coded
-		String[] anim = new String[]{"PlayerUp0.png", "PlayerUp1.png", "PlayerUp2.png", "PlayerRight0.png", "PlayerRight1.png", "PlayerRight2.png",
-				"PlayerDown0.png", "PlayerDown1.png", "PlayerDown2.png", "PlayerLeft0.png", "PlayerLeft1.png", "PlayerLeft2.png"};
-//		String[] anim = new String[]{"PlayerUp0.png", "PlayerRight0.png", "PlayerDown0.png", "PlayerLeft0.png"};
+		String[] anim = new String[] { "PlayerUp0.png", "PlayerUp1.png",
+				"PlayerUp2.png", "PlayerRight0.png", "PlayerRight1.png",
+				"PlayerRight2.png", "PlayerDown0.png", "PlayerDown1.png",
+				"PlayerDown2.png", "PlayerLeft0.png", "PlayerLeft1.png",
+				"PlayerLeft2.png" };
+		// String[] anim = new String[]{"PlayerUp0.png", "PlayerRight0.png",
+		// "PlayerDown0.png", "PlayerLeft0.png"};
 		addPlayer(anim, 2, 1, 1);
 
 	}
-
-
 }
