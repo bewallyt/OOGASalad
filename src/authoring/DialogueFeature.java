d7cf6b7a2540af993e136800c02493a988a075d5
Jacob Lettie <jrl47@duke.edu>
2014-04-13 20:20:44 -0400

    Implemented initial root text additions.

--- src/authoring/DialogueFeature.java
+++ src/authoring/DialogueFeature.java
@@ -2,9 +2,13 @@ package authoring;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -26,11 +30,10 @@ public class DialogueFeature extends Feature{
 	private JFrame frame;
 	public DialogueFeature(GridObjectCreation gridObjectCreation) {
 		mySuperFeature = gridObjectCreation;
-		myRoot = new NPCResponseNode("Enter NPCResponse Here");
+		myRoot = new NPCResponseNode("Initial NPC Response");
 		myCurrent = myRoot;
+		myResponsesWrapper = new JList();
 		setResponses();
-		myResponses.add(0,myCurrent.getString());
-		myResponsesWrapper = new JList(myResponses.toArray());
 		myResponsesWrapper.addListSelectionListener(new DialogueClickAction());
 		myResponsesWrapper.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		myTextWindow = new JScrollPane(myResponsesWrapper);
@@ -39,9 +42,11 @@ public class DialogueFeature extends Feature{
 	}
 	private void setResponses(){
 		myResponses = new ArrayList<String>();
+		myResponses.add(0,myCurrent.getString());
 		for(UserQueryNode q: myCurrent.getChildren()){
 			myResponses.add(q.getString());
 		}
+		myResponsesWrapper.setListData(myResponses.toArray());
 	}
 	public NPCResponseNode getDialogue() {
 		return myRoot;
@@ -49,11 +54,13 @@ public class DialogueFeature extends Feature{
 	private class DialogueClickAction implements ListSelectionListener{
 		public void valueChanged(ListSelectionEvent arg0) {
 			if(myResponsesWrapper.getSelectedValue()!=null){
-				frame = new JFrame("Input Dialogue");
-				frame.add(new DialogueInputPanel());
-	            frame.pack();
-	            frame.setVisible(true);
-				myResponsesWrapper.removeSelectionInterval(myResponsesWrapper.getSelectedIndex(), myResponsesWrapper.getSelectedIndex());
+				if(myResponsesWrapper.getSelectedIndex()==0){
+					frame = new JFrame("Input Dialogue");
+					frame.add(new DialogueInputPanel());
+					frame.pack();
+					frame.setVisible(true);
+					myResponsesWrapper.removeSelectionInterval(myResponsesWrapper.getSelectedIndex(), myResponsesWrapper.getSelectedIndex());
+				}
 			}
 		}
 		
@@ -62,12 +69,24 @@ public class DialogueFeature extends Feature{
 			
 			private JTextArea text;
 			private JScrollPane textWrapper;
+			
+			private JButton confirm;
 			public DialogueInputPanel(){
 				dialogueLabel = new JLabel("Enter the desired dialogue.");
 				text = new JTextArea(1,40);
 				textWrapper = new JScrollPane(text);
+				confirm = new JButton("Done");
+				confirm.addActionListener(new DoneListener());
 				this.add(dialogueLabel);
 				this.add(textWrapper);
+				this.add(confirm);
+			}
+			private class DoneListener implements ActionListener{
+				public void actionPerformed(ActionEvent arg0) {
+					myCurrent.setString(text.getText());
+					setResponses();
+					frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+				}
 			}
 		}
 	}
