bfbc565333455c2ba42ee532bbbf2ffe56afc49e
bchao <brandonchao3@gmail.com>
2014-04-19 01:46:53 -0400

    trying to add player to world

--- src/GameView/GameFrame.java
+++ src/GameView/GameFrame.java
@@ -1,4 +1,3 @@
-
 package GameView;
 
 import java.io.IOException;
@@ -35,9 +34,6 @@ import engine.gridobject.person.Enemy;
 
 public class GameFrame extends RPGEngine {
 
-	// temporary, will be removed when data adds this info into WorldData
-	private final int DEFAULT_MOVEMENT_TYPE = 1;
-	private final int DEFAULT_MOVEMENT_SPEED = 1;
 	private WorldData myWorldData;
 	// private DataManager myData;
 	private FileStorer myData;
@@ -49,14 +45,14 @@ public class GameFrame extends RPGEngine {
 		myData = new FileStorer();
 
 	}
-	
+
 	public void initialize(String fileName) {
 		try {
 			myWorldData = myData.getWorldData(fileName);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		initializeGame();
 	}
 
@@ -71,58 +67,62 @@ public class GameFrame extends RPGEngine {
 	 * Communication between Data and Engine test below: makeOutsideWorld()
 	 * addPlayer() addEnemy()
 	 */
-	
+
 	public void createWorlds() {
 		createPlayer();
-		for(MapData map : myWorldData.getMaps().values()) {
-			
+		for (MapData map : myWorldData.getMaps().values()) {
+
 			MapDataParser parser = new MapDataParser(map, myPlayer);
 			List<GridObject> gridObjectList = parser.getGridObjectList();
-						
+			gridObjectList.add(myPlayer);
 			List<String> TileImageList = parser.getTileImageList();
 
 			// tile size is default. ask engine to take it out of constructor
-			WalkAroundWorld currWorld = new WalkAroundWorld(map.getMapLength()*40, map.getMapWidth()*40, myPlayer, Constants.TILE_SIZE, gridObjectList);
+			WalkAroundWorld currWorld = new WalkAroundWorld(
+					map.getMapLength() * 40, map.getMapWidth() * 40, myPlayer,
+					Constants.TILE_SIZE, gridObjectList);
 			setWorld(currWorld);
-			
+
 			setGridObjects(currWorld, gridObjectList);
+			currWorld.setTileObject(myPlayer, 5, 0);
+
 			setTileImages(currWorld, TileImageList);
 		}
 	}
 
 	public void createPlayer() {
-		
+
 		PlayerData myPlayerData = myWorldData.getPlayData();
-		
+
 		String[] anim = new String[] { "PlayerUp0.png", "PlayerUp1.png",
 				"PlayerUp2.png", "PlayerRight0.png", "PlayerRight1.png",
 				"PlayerRight2.png", "PlayerDown0.png", "PlayerDown1.png",
 				"PlayerDown2.png", "PlayerLeft0.png", "PlayerLeft1.png",
 				"PlayerLeft2.png" };
-		
-		//String[] anim = myPlayerData.getMyAnimImages();
-		//int speed = myPlayerData.getSpeed();
-		//myPlayer = new Player(anim, speed);
+
+		// String[] anim = myPlayerData.getMyAnimImages();
+		// int speed = myPlayerData.getSpeed();
+		// myPlayer = new Player(anim, speed);
 		myPlayer = new Player(anim, 2);
 	}
 
 	public void setGridObjects(WalkAroundWorld world, List<GridObject> list) {
 		for (GridObject g : list) {
 			world.setTileObject(g, g.getX(), g.getY());
-			System.out.println("x: "+g.getX()+" y: "+g.getY());
+			System.out.println("x: " + g.getX() + " y: " + g.getY());
 		}
 	}
-	
+
 	public void setTileImages(WalkAroundWorld world, List<String> list) {
 		int n = 0;
-		for(int i = 0; i < world.getTileGridHeight(); i++) {
-			for(int j = 0; j < world.getTileGridWidth(); j++) {
+		for (int i = 0; i < world.getTileGridHeight(); i++) {
+			for (int j = 0; j < world.getTileGridWidth(); j++) {
 				world.setTileImage(i, j, list.get(n));
 				n++;
 			}
 		}
 	}
-	
+
 	public void run() {
 
 	}
