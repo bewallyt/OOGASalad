07d194170ba3f67b02715bed8b8d1f6a226d98a6
lee.weisberger <lee.weisberger@duke.edu>
2014-04-20 19:40:32 -0400

    added all calculations to battlecalculate

--- src/engine/battle/BattleManager.java
+++ src/engine/battle/BattleManager.java
@@ -23,6 +23,7 @@ import engine.gridobject.person.Player;
 import engine.item.Item;
 
 public class BattleManager implements InteractionBox{
+	BattleCalculate myBattleCalculate;
 	private Player myPlayer;
 	private Enemy myEnemy;
 	private BattleAI myBattleAI;
@@ -35,12 +36,11 @@ public class BattleManager implements InteractionBox{
 	private BattleExecutorNode myCurrentBattleExecutorNode;
 	private final static int TOPLEVEL=0;
 	private final static int BOTTOMLEVEL=1;
-	//private final static int ATTACKHAPPENED=2;
 	private final static int FIRSTATTACKHAPPENED=2;
 	private final static int SECONDATTACKHAPPENED=3;
 	private final static int WEAPONSELECTED=4;
 	private final static int RAN=5;
-	private final static int ENEMYDEAD=6;
+	public final static int ENEMYDEAD=6;
 	private final static int BATTLEDONE=7;
 	private static int myCurrentState=0;
 	private Person myCurrentAttacker;
@@ -55,7 +55,6 @@ public class BattleManager implements InteractionBox{
 	public static final int EXIT = 8;
 	private String textToBeDisplayed;
 	private boolean ran=false;
-	private int damageDealt;
 	private boolean dropWeapon = false;
 
 	public BattleManager(Player player, Enemy enemy){
@@ -115,51 +114,6 @@ public class BattleManager implements InteractionBox{
 		node.setChild(executorNode);
 
 	}
-
-
-
-	public void attack(Person attacker, Person victim, Weapon weapon, Attack attack){
-		int level = attacker.getStatsMap().get("level").getValue();
-		int playerDamage = attacker.getStatsMap().get("damage").getValue();
-		int weaponDamage = weapon.getDamage().getValue();
-		int attackDamage = attack.getDamage().getValue();
-		int defense = victim.getStatsMap().get("defense").getValue();
-		int random = 30 + (int)(Math.random() * ((25 - 30) + 1));
-		int total = (((((2*level+2)*playerDamage*(weaponDamage+attackDamage)/defense))+2)/random);
-		if(attackDamage!=0)
-			victim.getStatsMap().get("health").changeValue(-total);
-		if(attack.getEffect()!=null){
-			attack.getEffect().doEffect();
-		}
-		damageDealt=total;
-		if(myCurrentVictim.getStatsMap().get("health").getValue()<=0){
-			if (myCurrentVictim instanceof Enemy) {
-				dropWeapon = checkDropWeaponStatus();
-				if (dropWeapon) {
-					Weapon enemyWeapon = ((Enemy) myCurrentVictim).getWorld().getDroppedWeapon();
-					enemyWeapon.pickUp(myPlayer);
-				}
-			}
-			myCurrentState=ENEMYDEAD;
-		}
-	}
-
-	public Person[] attackFirst(Person person1, Weapon weapon1, Attack attack1, Person person2, Weapon weapon2, Attack attack2){
-		if(calcSpeed(person1,weapon1,attack1)>=calcSpeed(person2,weapon2,attack2))
-			return new Person[] {person1,person2};
-		return new Person[] {person2,person1};
-	}
-
-	public int calcSpeed(Person person, Weapon weapon, Attack attack){
-		return (person.getStatsMap().get("speed").getValue()+weapon.getSpeed().getValue()+attack.getSpeed().getValue());
-	}
-
-	public void chooseEnemyMove(){
-		Weapon weapon = myBattleAI.chooseWeapon();
-		Attack attack = myBattleAI.chooseAttack(weapon);
-	}
-
-
 
 	@Override
 	public void paintDisplay(Graphics2D g2d, int xSize, int ySize, int width,int height) {
@@ -227,7 +181,9 @@ public class BattleManager implements InteractionBox{
 			myCurrentAttacker=myCurrentVictim;
 			myCurrentVictim=tempAttacker;
 			setCurrentTextToBeDisplayed();
-			attack(myCurrentAttacker,myCurrentVictim,myCurrentAttacker.getCurrentWeapon(),myCurrentAttacker.getCurrentAttack());
+			myBattleCalculate.attack(myCurrentAttacker,myCurrentVictim,myCurrentAttacker.getCurrentWeapon(),myCurrentAttacker.getCurrentAttack());
+			if(myBattleCalculate.enemyIsDead())
+				myCurrentState=ENEMYDEAD;
 			myCurrentState=SECONDATTACKHAPPENED;
 
 		}
@@ -239,19 +195,22 @@ public class BattleManager implements InteractionBox{
 				setCurrentTextToBeDisplayed();
 			}
 			else if(executable instanceof Attack){
+				myBattleCalculate=new BattleCalculate(myPlayer, myEnemy);
 				Weapon enemyWeapon = myBattleAI.chooseWeapon();
 				myEnemy.setCurrentWeapon(enemyWeapon);
 				myEnemy.setCurrentAttack(myBattleAI.chooseAttack(enemyWeapon));
 				myPlayer.setCurrentAttack((Attack) executable);
 
-				myCurrentAttacker = attackFirst(myPlayer, myPlayer.getCurrentWeapon(), 
+				myCurrentAttacker = myBattleCalculate.attackFirst(myPlayer, myPlayer.getCurrentWeapon(), 
 						(Attack) executable, myEnemy, enemyWeapon, myEnemy.getCurrentAttack())[0];
-				myCurrentVictim = attackFirst(myPlayer, myPlayer.getCurrentWeapon(), 
+				myCurrentVictim = myBattleCalculate.attackFirst(myPlayer, myPlayer.getCurrentWeapon(), 
 						(Attack) executable, myEnemy, enemyWeapon, myEnemy.getCurrentAttack())[1];
+				
 				myCurrentState=FIRSTATTACKHAPPENED;
 				setCurrentTextToBeDisplayed();
-				attack(myCurrentAttacker,myCurrentVictim,myCurrentAttacker.getCurrentWeapon(),myCurrentAttacker.getCurrentAttack());
-
+				myBattleCalculate.attack(myCurrentAttacker,myCurrentVictim,myCurrentAttacker.getCurrentWeapon(),myCurrentAttacker.getCurrentAttack());
+				if(myBattleCalculate.enemyIsDead())
+					myCurrentState=ENEMYDEAD;
 				
 			}
 			else if(executable instanceof Item)
@@ -338,14 +297,7 @@ public class BattleManager implements InteractionBox{
 	public int getCurrentState(){
 		return myCurrentState;
 	}
-	private boolean checkDropWeaponStatus(){
-		boolean dropWeapon = false;
-		Random rand = new Random();
-		int randValue = rand.nextInt(10) + 1;
-		System.out.println("randvalue : " + randValue);
-		if (randValue < 7) {
-			dropWeapon = true;
-		}
-		return dropWeapon;
+	public void setCurrentState(int state){
+		myCurrentState=state;
 	}
 }
