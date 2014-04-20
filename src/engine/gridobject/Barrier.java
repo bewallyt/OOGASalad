ccada2dd3ad3e90b0efd80a242bf6343aa7330db
bchao <brandonchao3@gmail.com>
2014-04-20 00:39:21 -0400

    Changed constructors for reflection

--- src/engine/gridobject/Barrier.java
+++ src/engine/gridobject/Barrier.java
@@ -1,7 +1,14 @@
 package engine.gridobject;
 
-public class Barrier extends GridObject{
+import java.util.List;
+
+public class Barrier extends GridObject {
+	
 	public Barrier(String image, int numTilesWidth, int numTilesHeight) {
-		super(image, numTilesWidth, numTilesHeight);	
+		super(image, numTilesWidth, numTilesHeight);
+	}
+
+	public Barrier(List<Object> list) {
+		super((String) list.get(0), (int) list.get(2), (int) list.get(3));
 	}
 }
