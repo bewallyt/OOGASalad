469b2ba0924b9330ca7ad2a4c1532b93c4ffdada
Jacob Lettie <jrl47@duke.edu>
2014-04-25 17:52:09 -0400

    Implemented tile coordinate display.

--- src/authoring/GridObject.java
+++ src/authoring/GridObject.java
@@ -5,34 +5,34 @@ import java.util.List;
 
 
 public class GridObject {
-
-	protected String myImage;
-	protected int x;
-	protected int y;
-	private List<Item> itemList;
-	
-	public GridObject(int x, int y, String image){
-		this.x=x;
-		this.y=y;
-		myImage=image;
-        itemList=new ArrayList<Item>();
-	}
-	public void init(){
-		FeatureManager.getWorldData().getCurrentMap().getTileData(x,y).addGridObjectData(this);
-	}
-
-	public String getImageName(){
-		return myImage;
-	}
-	public int getX(){
-		return x;
-	}
-	public int getY(){
-		return y;
-	}
-    public List<Item> getItemList(){ return itemList;}
-	public void addItem(Item i){
-		itemList.add(i);
-	}
-}
-
+//
+//	protected String myImage;
+//	protected int x;
+//	protected int y;
+//	private List<Item> itemList;
+//	
+//	public GridObject(int x, int y, String image){
+//		this.x=x;
+//		this.y=y;
+//		myImage=image;
+//        itemList=new ArrayList<Item>();
+//	}
+//	public void init(){
+//		FeatureManager.getWorldData().getCurrentMap().getTileData(x,y).addGridObjectData(this);
+//	}
+//
+//	public String getImageName(){
+//		return myImage;
+//	}
+//	public int getX(){
+//		return x;
+//	}
+//	public int getY(){
+//		return y;
+//	}
+//    public List<Item> getItemList(){ return itemList;}
+//	public void addItem(Item i){
+//		itemList.add(i);
+//	}
+//}
+//
