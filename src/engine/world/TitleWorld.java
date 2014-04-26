0eb25edb2690a782daf386c0a0048040f8f088f1
Benson <benson.tran@duke.edu>
2014-04-26 00:45:43 -0400

    fixed title screen resolution

--- src/engine/world/TitleWorld.java
+++ src/engine/world/TitleWorld.java
@@ -8,14 +8,13 @@ import engine.images.ScaledImage;
 
 public class TitleWorld extends World {
 
-	private int myWidth;
-	private int myHeight;
+	private int myWidth = 500;
+	private int myHeight = 500;
 	private Image myBackground;
 
 	public TitleWorld(int playWidth, int playHeight, Player p) {		
 		super(playWidth, playHeight, p);
-		myWidth = playWidth;
-		myHeight = playHeight;
+
 	}
 
 
--- src/main/MainInGameTest.java
+++ src/main/MainInGameTest.java
@@ -1,25 +1,13 @@
 package main;
 
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import authoring.WorldData;
 import engine.gridobject.person.Player;
-import engine.item.Item;
-import engine.item.KeyItem;
-import engine.item.StatBuffer;
 import engine.main.RPGEngine;
-import engine.world.ArenaWorld;
 import engine.world.TitleWorld;
-import engine.world.WalkAroundWorld;
-import engine.world.World;
 
 public class MainInGameTest extends RPGEngine {
 	private WorldData myWorldData;
-
-
 
 	public static void main(String[] args) {
 		MainInGameTest engine = new MainInGameTest();
@@ -31,19 +19,18 @@ public class MainInGameTest extends RPGEngine {
 		}
 	}
 	
-	private void makeOutsideWorld(){
+	private void makeTitleScreen(){
 
-		String[] anim = new String[]{"PlayerUp0.png", "PlayerUp1.png", "PlayerUp2.png", 
-				"PlayerRight0.png", "PlayerRight1.png", "PlayerRight2.png",
-				"PlayerDown0.png", "PlayerDown1.png", "PlayerDown2.png", "PlayerLeft0.png", 
-				"PlayerLeft1.png", "PlayerLeft2.png"};
+//		String[] anim = new String[]{"PlayerUp0.png", "PlayerUp1.png", "PlayerUp2.png", 
+//				"PlayerRight0.png", "PlayerRight1.png", "PlayerRight2.png",
+//				"PlayerDown0.png", "PlayerDown1.png", "PlayerDown2.png", "PlayerLeft0.png", 
+//				"PlayerLeft1.png", "PlayerLeft2.png"};
+//
+//		Player player = new Player(anim, "Player", 2, new String[1], new String[1]);
 
-		Player player = new Player(anim, "Player", 2, new String[1], new String[1]);
-
-		TitleWorld titleScreen = new TitleWorld(1000, 1000, player);
+		TitleWorld titleScreen = new TitleWorld(1000, 1000, null);
 		titleScreen.setBackground("PokemonBackground.png");
-		setWorld(titleScreen); // this is only called for the initial world
-		
+		setWorld(titleScreen);
 		//titleScreen.setMusic("/music/pokeTest.wav");
 }
 
@@ -52,6 +39,6 @@ public class MainInGameTest extends RPGEngine {
 		setInit(true);
 		//initMusicTest();
 		initializeCanvas(500, 500);
-		makeOutsideWorld();
+		makeTitleScreen();
 	}
 }
