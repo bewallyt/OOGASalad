889eb73cd837d856ac4523c79c4c0de6d2ef1835
Davis Treybig <davis.treybig@gmail.com>
2014-04-26 17:21:05 -0400

    Music management

---
+++ src/Data/MusicManager.java
@@ -0,0 +1,60 @@
+package Data;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class MusicManager extends AbstractFileManager{
+
+	public static final String DEFAULT_MUSIC_EXTENSION="wav";
+	public static final String[] VALID_MUSIC_EXTENSIONS={".mp3", ".wav"};
+	public static final String DEFAULT_MUSIC_FOLDER=SRC+"Music/";
+	public MusicManager() {
+		super(VALID_MUSIC_EXTENSIONS, DEFAULT_MUSIC_EXTENSION);
+	}
+	/**
+	 * 
+	 * @param fileName Name of the song
+	 * @param musicFile File from which to copy the song
+	 * @return Returns the file within the project of the newly copied song
+	 * @throws IOException
+	 */
+	public File storeMusicFile(String fileName, File musicFile) throws IOException{
+		String extension=getFileExtension(musicFile);
+		if(checkFileExtension(extension)){
+			Path source=Paths.get(musicFile.getAbsolutePath());
+			String newParentPath=DEFAULT_MUSIC_FOLDER;
+			String newPath=newParentPath+fileName+extension;
+			
+			Path newDirectory=Paths.get(newPath);
+			File newImageFile=new File(newPath);
+			File parentFolderFile=new File(newParentPath);
+			parentFolderFile.mkdirs();
+			try{
+				Files.copy(source, newDirectory);
+				System.out.println("Tester");
+			}
+			catch(FileAlreadyExistsException e){
+				return null;
+			}
+			return (newImageFile);
+		}
+		return null;
+	}
+	
+
+	public static void main(String[] args) throws IOException{
+		MusicManager m=new MusicManager();
+		JFileChooser chooser=new JFileChooser();
+		int returnVal=chooser.showOpenDialog(null);
+		File f=chooser.getSelectedFile();
+		m.storeMusicFile("testSong", f);
+	}
+
+}
