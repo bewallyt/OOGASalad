cde4d6b5dc20ec97600609dfeb932d913df84ee4
lee.weisberger <lee.weisberger@duke.edu>
2014-04-25 20:48:36 -0400

    healer implemented

--- src/engine/gridobject/person/Healer.java
+++ src/engine/gridobject/person/Healer.java
@@ -2,19 +2,29 @@ package engine.gridobject.person;
 
 import java.util.List;
 
+import engine.dialogue.NPCResponseNode;
+import engine.dialogue.UserQueryNode;
+
 public class Healer extends NPC {
 
 	public Healer(String[] animImages, String name, double speed, int numTilesWidth,
 			int numTilesHeight, int movementType, Player player) {
 		super(animImages, name, speed, numTilesWidth, numTilesHeight, movementType, player);
-	}
-	
-//	public Healer(List<Object> list) {
-//		super((String[]) list.get(1), (int) list.get(4), (int) list.get(2),
-//				(int) list.get(3), (int) list.get(5), (Player) list.get(6));
-//	}
+		NPCResponseNode n = new NPCResponseNode(this, "I will heal you.");
+		
 
-	
+		
+
+
+		this.setResponseNode(n);
+	}
+
+	//	public Healer(List<Object> list) {
+	//		super((String[]) list.get(1), (int) list.get(4), (int) list.get(2),
+	//				(int) list.get(3), (int) list.get(5), (Player) list.get(6));
+	//	}
+
+
 	@Override
 	public void doAction(){
 		doDialogue();
