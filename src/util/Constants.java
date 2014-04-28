26197d4afb414a67fee5ce90ec598c4bce07aa73
bchao <brandonchao3@gmail.com>
2014-04-28 12:06:57 -0400

    Shopkeeper

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -85,7 +85,8 @@ public class Constants {
     public static final int NPC_ITEMS_CONST = 7;
     public static final int ENEMY_PLAYER_CONST = 8;
     public static final int SHOP_PLAYER_CONST = 5;
-    public static final int SHOP_ITEMS_CONST = 5;
+    public static final int SHOP_ITEMS_CONST = 4;
+    public static final int SHOP_ITEMSMAP_CONST = 6;
 
     
     
