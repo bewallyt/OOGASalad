c6bdfe7d7cb621db8ebcdd645f643b4069af076c
Benson <benson.tran@duke.edu>
2014-05-01 04:30:19 -0400

    pretty points

--- src/GameView/GameChooserWorld.java
+++ src/GameView/GameChooserWorld.java
@@ -1,6 +1,7 @@
 package GameView;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 
@@ -19,6 +20,9 @@ public class GameChooserWorld extends TitleWorld implements InteractionBox {
 	private InteractionMatrix2x2 matrix2;
 	private String[][] myGames = new String[2][2];
 	private String myGameString;
+	private int increaseY;
+	private int increaseX;
+	private boolean isExpanding = false;
 
 	public GameChooserWorld(int playWidth, int playHeight, Player p,
 			String chooseScreen) {
@@ -28,14 +32,26 @@ public class GameChooserWorld extends TitleWorld implements InteractionBox {
 		myGames[0][1] = "Other";
 		myGames[1][1] = "FinalFantasy";
 		matrix2 = new InteractionMatrix2x2();
+		increaseY = 0;
+		increaseX = 0;
 
 	}
 
 	@Override
 	public void paintDisplay(Graphics2D g, int xSize, int ySize, int xOffset,
 			int yOffset) {
+		if (isExpanding) {
+			increaseY+=2;
+			increaseX+=2;
+		}
 		PokemonFont.setFont(g, 16f);
+		if(!isExpanding){
 		drawSelector(g, 0, 0, 250, 250, 250);
+		}
+		else if ((matrix2.getSelectedNodeLocation()[1] == 0) && (matrix2.getSelectedNodeLocation()[0] == 1)){
+			
+			expandImage(g, "ImageFiles/PokemonTitle.png", 1, 0, increaseX, increaseY);
+		}
 
 	}
 
@@ -107,7 +123,26 @@ public class GameChooserWorld extends TitleWorld implements InteractionBox {
 	}
 
 	public void setGameString() {
-		myGameString = matrix2.getCurrentNode().getGame();
+		isExpanding = true;
+		System.out.println(isExpanding);
+		if (increaseY > 500) {
+			myGameString = matrix2.getCurrentNode().getGame();
+		}
+	}
+
+	private void expandImage(Graphics g2d, String imageFile, int xPos,
+			int yPos, int increaseX, int increaseY) {
+		if ((xPos == 1) && (yPos == 0)) {
+			System.out.println("expanding");
+			Image img = new ScaledImage(250 + increaseX, 250 + increaseY,
+					imageFile).scaleImage();
+			g2d.drawImage(img, 250-increaseX, 0, null);
+		}
+		// else if((xPos == 0) && (yPos == 1)){
+		// Image img = new ScaledImage(250 + increaseX, 250 + increaseY,
+		// "ImageFiles/PokemonTitle.png").scaleImage();
+		// g2d.drawImage(img, xPos, yPos, null);
+		// }
 	}
 
 	public String getGameString() {
